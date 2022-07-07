d4e476ebb565af6f6f62cc5041b3ebc84415bee0
Straivers <straivers98@gmail.com>
2022-07-06 22:34:30 -0400

    First window
    Fathom attempt number 2.
    
    Important lessons learned:
    
    - Rust appears to favour building bottom-up, iteratively adding features and state as client code develops. I was not doing this, and it resulted in weeks of deadlock as I attempted to figure out what to do.
    - Super-clear, well defined steps are required _before_ each stage of development (even for a single commit). It's okay to defer 'just a second' changes like a small bugfix to a separate branch off main that gets merged quickly.
    - As expensive as dynamic allocation may be, it's typically cheaper on development time. Treat it like any other optimization, because that's what it is.
      - Note, to_wstr is the way it is because I explicitly plan to create/modify multiple windows on the same thread. Using a shared
    
    This makes use of ArrayVec, which is used in a couple of other projects of mine. It should probably be broken out into a separate crate along with other utilities, though I know of at least one other arrayvec crate on crates.io. Perhaps if additional utilities find their way together?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,137 @@
-fn main() {
-    println!("Hello, world!");
+
+use windows::{
+    core::PCWSTR,
+    Win32::{
+        Foundation::{GetLastError, HWND, LPARAM, LRESULT, WPARAM},
+        System::LibraryLoader::GetModuleHandleW,
+        UI::WindowsAndMessaging::{
+            CreateWindowExW, DefWindowProcW, DispatchMessageW, GetMessageW, LoadCursorW,
+            PeekMessageW, PostQuitMessage, RegisterClassExW, ShowWindow, TranslateMessage,
+            CS_HREDRAW, CS_VREDRAW, CW_USEDEFAULT, IDC_ARROW, MSG, PM_REMOVE, SW_SHOW,
+            WINDOW_EX_STYLE, WM_DESTROY, WM_QUIT, WNDCLASSEXW, WS_OVERLAPPEDWINDOW,
+        },
+    },
+};
+
+const WINDOW_TITLE: &str = "Hello!";
+
+/// The name of Fathom's window classes `"FATHOM_WNDCLASS"` in UTF-16 as an
+/// array of `u16`s.
+const WNDCLASS_NAME: &[u16] = &[
+    0x0046, 0x0041, 0x0054, 0x0048, 0x004f, 0x004d, 0x005f, 0x0057, 0x004e, 0x0044, 0x0043, 0x004c,
+    0x0041, 0x0053, 0x0053,
+];
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let hinstance = unsafe { GetModuleHandleW(None)? };
+
+    let _wndclass_atom = {
+        let arrow_cursor = unsafe { LoadCursorW(None, &IDC_ARROW)? };
+
+        let wndclass = WNDCLASSEXW {
+            cbSize: std::mem::size_of::<WNDCLASSEXW>().try_into().unwrap(),
+            style: CS_VREDRAW | CS_HREDRAW,
+            hInstance: hinstance,
+            lpfnWndProc: Some(wndproc),
+            lpszClassName: PCWSTR(WNDCLASS_NAME.as_ptr()),
+            hCursor: arrow_cursor,
+            ..WNDCLASSEXW::default()
+        };
+
+        unsafe { RegisterClassExW(&wndclass) }
+    };
+
+    let hwnd = {
+        let os_title = wide_title(WINDOW_TITLE);
+
+        unsafe {
+            CreateWindowExW(
+                WINDOW_EX_STYLE::default(),
+                PCWSTR(WNDCLASS_NAME.as_ptr()),
+                PCWSTR(os_title.as_ptr()),
+                WS_OVERLAPPEDWINDOW,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                None,
+                None,
+                hinstance,
+                std::ptr::null_mut(),
+            )
+        }
+    };
+
+    unsafe { ShowWindow(hwnd, SW_SHOW) };
+
+    'event_pump: loop {
+        let mut msg = MSG::default();
+        let ret = unsafe { GetMessageW(&mut msg, None, 0, 0).0 };
+        if ret == -1 {
+            panic!("GetMessage failed. Error: {:?}", unsafe { GetLastError() });
+        } else if ret == 0 {
+            break;
+        } else {
+            unsafe {
+                TranslateMessage(&msg);
+                DispatchMessageW(&msg);
+            }
+        }
+
+        while unsafe { PeekMessageW(&mut msg, None, 0, 0, PM_REMOVE) }.into() {
+            if msg.message == WM_QUIT {
+                break 'event_pump;
+            }
+
+            unsafe {
+                TranslateMessage(&msg);
+                DispatchMessageW(&msg);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+unsafe extern "system" fn wndproc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    match msg {
+        WM_DESTROY => {
+            PostQuitMessage(0);
+            LRESULT::default()
+        }
+        _ => DefWindowProcW(hwnd, msg, wparam, lparam),
+    }
+}
+
+/// Converts a string into a `Vec<u16>` of wide characters.
+/// 
+/// Note (straivers): For the moment, this allocates memory on the heap.
+/// However, if things go as I expect once multiple windows are supported with
+/// their associated per-frame bump allocators, these can be allocated from
+/// there instead.
+fn wide_title(title: &str) -> Vec<u16> {
+    use std::{ffi::OsStr, os::windows::prelude::OsStrExt};
+
+    let mut buffer: Vec<u16> = OsStr::new(title).encode_wide().collect();
+    buffer.push(0);
+
+    buffer
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn wstr_conversion() {
+        use std::{ffi::OsStr, os::windows::prelude::OsStrExt};
+
+        let s = "Too many bagels tonight";
+        let wstr = wide_title(s);
+        let os_str: Vec<u16> = OsStr::new(s).encode_wide().collect();
+
+        // Check that the conversion is correct, accounting for the null
+        // terminator in `to_wstr()`.
+        assert_eq!(&wstr.as_slice()[0..wstr.len() - 1], &os_str[..]);
+    }
 }
